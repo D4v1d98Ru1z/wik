f8d5452459e3a267c1692e88f22520caa0c35a41
Logan Tran <loganmartintran@icloud.com>
2017-06-13 22:50:58 -0500

    implement new and create action and views

--- app/controllers/wikis_controller.rb
+++ app/controllers/wikis_controller.rb
@@ -7,6 +7,21 @@ class WikisController < ApplicationController
   end
 
   def new
+    @wiki = Wiki.new
+  end
+
+  def create
+    @wiki = Wiki.new
+    @wiki.title = params[:wiki][:title]
+    @wiki.body = params[:wiki][:body]
+
+    if @wiki.save
+      flash[:notice] = "Your wiki was saved successfully"
+      redirect_to @wiki
+    else
+      flash[:alert] = "There was an error saving your wiki. Please try again"
+      render :new
+    end
   end
 
   def edit
--- spec/controllers/wikis_controller_spec.rb
+++ spec/controllers/wikis_controller_spec.rb
@@ -22,12 +22,38 @@ RSpec.describe WikisController, type: :controller do
   #   end
   # end
   #
-  # describe "GET #new" do
-  #   it "returns http success" do
-  #     get :new
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #new" do
+    it "returns http success" do
+      get :new
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #new view" do
+      get :new
+      expect(response).to render_template :new
+    end
+
+    it "instantiates a @wiki" do
+      get :new
+      expect(assigns(:wiki)).not_to be_nil
+    end
+  end
+
+  describe "POST #create" do
+    it "increases the number of Wikis by 1" do
+      expect {post :create, wiki: {title:"title", body: "body"}}.to change(Wiki,:count).by(1)
+    end
+
+    it "assigngs the new wiki to a @wiki" do
+      post :create, wiki: {title:"title", body: "body"}
+      expect(assigns(:wiki)).to eq Wiki.last
+    end
+
+    it "redirects to the new wiki" do
+      post :create, wiki: {title:"title", body: "body"}
+      expect(response).to redirect_to Wiki.last
+    end
+  end
   #
   # describe "GET #edit" do
   #   it "returns http success" do
