4de8d419bb86588ef8567696e6d7dc76e814d4c9
Logan Tran <loganmartintran@icloud.com>
2017-06-15 19:43:52 -0500

    add faker gem and seed db with fake data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+#Create 50 fake users
+50.times do
+  User.create!(email: Faker::Internet.email, password: Faker::Internet.password)
+end
+
+users = User.all
+
+#Create 100 fake wikis
+100.times do
+  Wiki.create!(title: Faker::Hipster.word, body: Faker::Hipster.paragraph(7), user: users.sample)
+end
+
+puts "Seed finished"
+puts "#{User.count} users created"
+puts "#{Wiki.count} wikis created"
