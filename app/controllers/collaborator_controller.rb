1c54012ecc3ed37aeaeb09a95e09acd61b947340
Logan Tran <loganmartintran@icloud.com>
2017-06-28 15:01:10 -0500

    attempt #2 at getting collaborators associacions to work

---
+++ app/controllers/collaborator_controller.rb
@@ -0,0 +1,11 @@
+class CollaboratorController < ApplicationController
+  def new
+    @collaborator = Collaborator.new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
---
+++ db/migrate/20170628195536_add_wiki_to_collaborator.rb
@@ -0,0 +1,5 @@
+class AddWikiToCollaborator < ActiveRecord::Migration
+  def change
+    add_reference :collaborators, :wiki, index: true, foreign_key: true
+  end
+end
