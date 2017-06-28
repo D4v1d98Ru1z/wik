1f15d9a9236dcb348582ffe8207d3fd595d3dc9f
Logan Tran <loganmartintran@icloud.com>
2017-06-27 20:28:49 -0500

    attempt to get associations working

--- app/models/wiki.rb
+++ app/models/wiki.rb
@@ -1,5 +1,5 @@
 class Wiki < ActiveRecord::Base
   belongs_to :user
-
+  has_many :collaborators
   validates :user, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170623000539) do
+ActiveRecord::Schema.define(version: 20170628010543) do
+
+  create_table "collaborators", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "collaborators", ["id"], name: "index_collaborators_on_id", unique: true
+  add_index "collaborators", [nil], name: "index_collaborators_on_user_id"
+  add_index "collaborators", [nil], name: "index_collaborators_on_wiki_id"
 
   create_table "users", force: :cascade do |t|
     t.datetime "created_at",                          null: false
@@ -33,6 +42,14 @@ ActiveRecord::Schema.define(version: 20170623000539) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+
+  create_table "users_wikis", id: false, force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "wiki_id", null: false
+  end
+
+  add_index "users_wikis", ["user_id", "wiki_id"], name: "index_users_wikis_on_user_id_and_wiki_id"
+  add_index "users_wikis", ["wiki_id", "user_id"], name: "index_users_wikis_on_wiki_id_and_user_id"
 
   create_table "wikis", force: :cascade do |t|
     t.string   "title"
