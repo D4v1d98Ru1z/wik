8fa8f5dd2b92ac2a2173c673d4d374a8d7a7514b
Logan Tran <loganmartintran@icloud.com>
2017-08-18 11:02:26 -0500

    fix some styling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,11 +17,11 @@
 //= require_tree .
 
 var metric = {};
-  metric.report= function(eventName) {
+  metric.report = function(eventName) {
     var event = {event: {name: eventName}};
 
     var request = new XMLHttpRequest();
     request.open("POST", "http://localhost:3000/api/events", true);
     request.setRequestHeader('Content-Type', 'application/json');
-    request.sent(JSON.stringify(event));
+    request.send(JSON.stringify(event));
   };
