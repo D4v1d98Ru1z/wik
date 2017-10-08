5051017504e58224ad74fcbcf29e71d1893c87c1
logan tran <loganmartintran@icloud.com>
2017-10-08 02:07:57 -0500

    refactor and complete create action

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -19,50 +19,50 @@ class ChargesController < ApplicationController
     nonce = 'fake-valid-nonce'
 
     # Search the Braintree vault to see if there is an exisitng customer matching the current_user email
-    collection = Braintree::Customer.search do |search|
+    customers = Braintree::Customer.search do |search|
       search.email.is "#{current_user.email}"
     end
 
-    my_customer = nil
-
-    collection.each do |customer|
-      my_customer = customer.email
-    end
-
-    # If there is not an exisiting customer, create a new customer in the vault along with a new transaction
-    # Otherwise, create a new transaction for the existing customer customer
-    if my_customer == nil
+    # If there is not an exisiting customer, create a new customer in the vault
+    # and save their payment method from the nonce
+    if customers.ids.empty?
       my_customer = Braintree::Customer.create(
-        email: current_user.email
-      ).customer
+        email: current_user.email,
+        payment_method_nonce: nonce,
+      )
+      
+      # Find the new customer that was newly created
+      my_new_customer = Braintree::Customer.find(my_customer.customer.id)
 
+      # assign the customer's payment method from the stored payment method token
+      customer_payment_method = my_new_customer.payment_methods.first.token
+
+    # Otherwise, assign the existing customer
     else
-      my_customer = Braintree::Customer.search do |search|
-        search.email.is "#{my_customer}"
-      end
-
-      my_customer = Braintree::Customer.find(my_customer.ids.first)
+      my_customer = Braintree::Customer.find(customers.ids.first)
+      customer_payment_method = Braintree::PaymentMethod.create(
+        customer_id: my_customer.id,
+        payment_method_nonce: nonce
+      )
     end
 
-    result = Braintree::Subscription.create(
+    # create a new subscription
+    customer_subscription = Braintree::Subscription.create(
       plan_id: "56q6",
-      descriptor: "Wik - Premium Plan Subscription",
-      customer_id: my_customer.id,
-      payment_method_nonce: nonce,
-      options: {
-        submit_for_settlement: true
-      }
+      payment_method_token: customer_payment_method.payment_method.token,
     )
 
-    if result.success? || result.transaction
-      puts "Transaction successful!: #{result.transaction.id}"
+    # if the subscription is successful, update the role, braintree_id, and braintree_subscription attributes
+    if customer_subscription.success?
+      puts "Subscription successful!: #{customer_subscription.subscription.id}"
       flash[:notice] = "Welcome to Premium! You can now enjoy creating private wikis!"
       current_user.update_attributes(role: 'premium')
       current_user.update_attributes(braintree_id: my_customer.id)
-      current_user.update_attributes(braintree_subscription: )
+      current_user.update_attributes(braintree_subscription: customer_subscription.subscription.id)
       redirect_to root_path
+    # otherwise raise the error messages and redirect to the new charge path
     else
-      error_messages = result.errors.map { |error| "Error: #{error.code}: #{error.message}" }
+      error_messages = customer_subscription.errors.map { |error| "Error: #{error.code}: #{error.message}" }
       puts error_messages
       flash[:alert] = error_messages
       redirect_to new_charge_path
